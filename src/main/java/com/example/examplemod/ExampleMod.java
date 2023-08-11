4abb8a4eebd266c04a1c9fe42c7cedbd06e31149
DemyCode <verycols@gmail.com>
2023-08-11 09:55:30 +0200

    fix: clean state, removing all

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,4 +1,4 @@
-package com.example.minecraft_copilot;
+package com.example.examplemod;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
@@ -30,11 +30,11 @@ import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(MinecraftCopilotMod.MODID)
-public class MinecraftCopilotMod
+@Mod(ExampleMod.MODID)
+public class ExampleMod
 {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "minecraft_copilot";
+    public static final String MODID = "examplemod";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
@@ -61,7 +61,7 @@ public class MinecraftCopilotMod
                 output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
             }).build());
 
-    public MinecraftCopilotMod()
+    public ExampleMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
