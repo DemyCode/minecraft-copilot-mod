cd4169efd894a9818ca482f0798d7610210ecd6a
DemyCode <verycols@gmail.com>
2023-08-12 14:01:52 +0200

    feat: init mod with correct ID + ONNX runtime dependencies

--- src/main/java/net/demycode/minecraft_copilot_mod/MinecraftCopilotMod.java
+++ src/main/java/net/demycode/minecraft_copilot_mod/MinecraftCopilotMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package net.demycode.minecraft_copilot_mod;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
@@ -28,10 +28,17 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
+import ai.djl.inference.*;
+import ai.djl.modality.*;
+import ai.djl.ndarray.*;
+import ai.djl.ndarray.types.*;
+import ai.djl.repository.zoo.*;
+import ai.djl.translate.*;
+import java.util.*;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
+@Mod(MinecraftCopilotMod.MODID)
+public class MinecraftCopilotMod
 {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "minecraft_copilot_mod";
@@ -61,7 +68,7 @@ public class ExampleMod
                 output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
             }).build());
 
-    public ExampleMod()
+    public MinecraftCopilotMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
